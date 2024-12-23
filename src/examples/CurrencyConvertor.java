e682997c95d4f3fd444cf186eb87cdb303a95485
hossam2000ayman <hossam2000.albadry@gmail.com>
2024-12-24 00:02:15 +0200

    first commit

---
+++ src/examples/CurrencyConvertor.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package examples;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author hossa
+ */
+public class CurrencyConvertor {
+    public static void main(String[] args) {
+        CurrencyFrame cf = new CurrencyFrame();
+        cf.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        cf.setVisible(true);
+    }
+}
